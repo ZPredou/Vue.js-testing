2f4d3774f97f68d987b266b7420e3576dba41f7c
Decourty William <williamdecourty@live.fr>
2018-05-29 15:38:10 +0200

    Trying out router with different componnent

---
+++ src/components/citizens.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <table>
+        <tr :key="citizen.id" v-for="citizen in citizens">
+            <td >{{ citizen.firstname }}</td>
+            <td>{{ citizen.lastname }}</td>           
+            <td>{{ citizen.age }}</td>           
+        </tr>
+    </table>
+    <button @click="addCitizen">Ajouter </button>
+    <label v-if="counter != 1">#citizens {{counter}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Citizen',
+  data () {
+      return{
+          citizens:[]
+      }
+    },
+    methods:{
+        addCitizen(){
+            this.citizens.push({"firstname":"William", "lastname":"Decourty", "age":"21"});
+        }
+    },
+    computed:{
+        counter(){
+            return this.citizens.length+1
+        }
+    },
+    mounted(){}
+}
+</script>
